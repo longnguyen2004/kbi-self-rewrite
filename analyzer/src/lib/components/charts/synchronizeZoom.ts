e76a603571227d17a43c18b2dfe9bc92509f40d4
Long Nguyen <nguyen.long.908132@gmail.com>
2025-06-25 10:03:04 +0700

    DRY on `ZoomSynchronizer` class

--- analyzer/src/lib/components/charts/synchronizeZoom.ts
+++ analyzer/src/lib/components/charts/synchronizeZoom.ts
@@ -1,5 +1,4 @@
 import type { Chart, ChartType, PluginOptionsByType } from "chart.js"
-import type {} from "chartjs-plugin-zoom"
 
 type ZoomPluginOptions = PluginOptionsByType<ChartType>["zoom"]
 type ZoomCallback = NonNullable<ZoomPluginOptions["zoom"]>["onZoom"];
@@ -11,48 +10,35 @@ export type SyncOptions = {
 
 export class ZoomSynchronizer {
     private _charts = new Map<Chart, SyncOptions>();
-    public onZoom: ZoomCallback = ({ chart: srcChart, trigger }) => {
+    private _doSync(srcChart: Chart) {
+        for (const [dstChart, syncOpts] of this._charts.entries()) {
+            if (dstChart === srcChart)
+                continue;
+            for (const srcAxis of Object.values(srcChart.scales)) {
+                const { id, axis, min, max } = srcAxis;
+                if (!syncOpts.axis.includes(id) && !syncOpts.axis.includes(axis))
+                    continue;
+                dstChart.zoomScale(id, { min, max });
+            }
+        }
+    }
+    public onZoom: ZoomCallback = ({ chart, trigger }) => {
         // do not trigger on api calls to prevent infinite loops
         if (trigger === "api")
             return;
-        for (const [dstChart, syncOpts] of this._charts.entries())
-        {
-            if (dstChart === srcChart)
-                continue;
-            for (const srcAxis of Object.values(srcChart.scales))
-            {
-                const { id, axis, min, max } = srcAxis;
-                if (!syncOpts.axis.includes(id) && !syncOpts.axis.includes(axis))
-                    continue;
-                dstChart.zoomScale(id, { min, max });
-            }
-        }
+        this._doSync(chart);
     }
-    public onPan: PanCallback = ({ chart: srcChart }) => {
+    public onPan: PanCallback = ({ chart }) => {
         // No need to check for API trigger here because pan isn't called again
-        for (const [dstChart, syncOpts] of this._charts.entries())
-        {
-            if (dstChart === srcChart)
-                continue;
-            for (const srcAxis of Object.values(srcChart.scales))
-            {
-                const { id, axis, min, max } = srcAxis;
-                if (!syncOpts.axis.includes(id) && !syncOpts.axis.includes(axis))
-                    continue;
-                dstChart.zoomScale(id, { min, max });
-            }
-        }
+        this._doSync(chart);
     }
-    add(chart: Chart, options: SyncOptions)
-    {
+    add(chart: Chart, options: SyncOptions) {
         this._charts.set(chart, options);
     }
-    remove(chart: Chart)
-    {
+    remove(chart: Chart) {
         this._charts.delete(chart);
     }
-    clear()
-    {
+    clear() {
         this._charts.clear();
     }
 }
