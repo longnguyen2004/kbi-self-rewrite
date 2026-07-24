6dc988084739ede811adbb21ef0ed39b21415ef1
Long Nguyen <nguyen.long.908132@gmail.com>
2026-07-24 15:28:19 +0700

    Update everything

--- analyzer/src/lib/components/ui/dialog/dialog-header.svelte
+++ analyzer/src/lib/components/ui/dialog/dialog-header.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import { cn, type WithElementRef } from "$lib/utils.js";
 	import type { HTMLAttributes } from "svelte/elements";
+	import { cn, type WithElementRef } from "$lib/utils.js";
 
 	let {
 		ref = $bindable(null),
 		class: className,
 		children,
 		...restProps
-	}: WithElementRef<HTMLAttributes<HTMLDivElement>> & {} = $props();
+	}: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
 </script>
 
 <div
 	bind:this={ref}
-	data-slot="select-label"
-	class={cn("text-muted-foreground px-1.5 py-1 text-xs", className)}
+	data-slot="dialog-header"
+	class={cn("gap-2 flex flex-col", className)}
 	{...restProps}
 >
 	{@render children?.()}
