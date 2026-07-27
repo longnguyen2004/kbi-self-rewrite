08a38b403d339eb5952916765255a230a6365848
Long Nguyen <nguyen.long.908132@gmail.com>
2026-07-27 23:54:39 +0700

    Expose WebSocket mode

--- recorder/src/main.cpp
+++ recorder/src/main.cpp
@@ -1,6 +1,7 @@
 #include <recorder.h>
 #include "controller/controller.h"
 #include <boost/di.hpp>
+#include <boost/program_options.hpp>
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
@@ -14,9 +15,22 @@
 #endif
 
 namespace di = boost::di;
+namespace po = boost::program_options;
 
 int main(int argc, char const *argv[])
 {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("websocket", "run in websocket mode");
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 0;
+    }
+
     const auto injector = di::make_injector(
         di::bind<spdlog::logger>.to([]() -> std::shared_ptr<spdlog::logger> {
             auto console_sink = std::make_shared<spdlog::sinks::stderr_color_sink_mt>();
@@ -38,7 +52,11 @@ int main(int argc, char const *argv[])
 
             return multi_sink;
         }),
-        di::bind<Controller>.to<ConsoleController>()
+        di::bind<Controller>.to([&](const auto& injector) -> Controller& {
+            if (vm.count("websocket"))
+                return injector.template create<WebSocketController&>();
+            return injector.template create<ConsoleController&>();
+        })
     );
     auto& controller = injector.create<Controller&>();
     controller.Run();
